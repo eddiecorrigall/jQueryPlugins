3b70b9418f31db9cb5ad5dd0ac267122713b7883
Edward <eddiecorrigall@gmail.com>
2014-10-15 21:34:49 -0400

    Initial commit

---
+++ jquery.deparam.js
@@ -0,0 +1,32 @@
+(function ($) {
+
+	$.deparam = function(url) { // Counterpart of $.param
+	
+		var parameters = {};
+		
+		var searchQueryIndex = url.indexOf('?');
+		
+		if (searchQueryIndex < 0) {
+			return "";
+		}
+		
+		var searchQuery = url.substr(searchQueryIndex + 1);
+		
+		$.each(searchQuery.split('&'), function(index, parameter) {
+			
+			var keyValuePair = parameter.split('=');
+			
+			var field = keyValuePair[0];
+			var value = keyValuePair[1];
+			
+			if (parameters.hasOwnProperty(field) == false) {
+				parameters[field] = [];
+			}
+			
+			parameters[field].push(value);
+		});
+		
+		return parameters;
+	};
+	
+}(jQuery));
