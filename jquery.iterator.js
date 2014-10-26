381dcb8be3abff336b3ffaf0956ceeb42f31b871
Edward <eddiecorrigall@gmail.com>
2014-10-26 14:11:52 -0400

    Changed variable scope to local.

--- jquery.iterator.js
+++ jquery.iterator.js
@@ -3,27 +3,27 @@
 	$.fn.iterateTable = function(callback) {
 
 		// Obtain the selected object
-		tableSelector = $(this);
+		var tableSelector = $(this);
 
 		// For each table element in selector...
 		$(tableSelector).each(function() {
 
-			tableElement = $(this);
+			var tableElement = $(this);
 
 			// Ignore if the element type is not a table
 			if (tableElement.is("table") == false) return; // aka continue
 
 			// Collect all table field names...
 
-			field_names = [];
-			fieldSelector = $("tr > th", tableElement);
+			var field_names = [];
+			var fieldSelector = $("tr > th", tableElement);
 
 			// Skip if their are no field names
 			if (fieldSelector.length == 0) return; // aka continue
 
 			fieldSelector.each(function(column_index) {
-				th = $(this); // table header object
-				field_name = th.text() || column_index;
+				var th = $(this); // table header object
+				var field_name = th.text() || column_index;
 				if (field_name.length == 0) field_name = column_index;
 				field_names[column_index] = field_name;
 			});
@@ -31,16 +31,16 @@
 			// For each table row...
 			$("tr", tableElement).each(function(row_index) {
 
-				tr = $(this); // table row object
+				var tr = $(this); // table row object
 
-				fields = {};
+				var fields = {};
 
 				// Buffer column data...
 				$("td", tr).each(function(column_index) {
 
-					td = $(this); // table data object
-
-					field_name = field_names[column_index];
+					var td = $(this); // table data object
+					
+					var field_name = field_names[column_index];
 					fields[ field_name ] = td;
 				});
 
