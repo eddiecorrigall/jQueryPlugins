b098effa2a5f3e9b13ee54174252cdc70e56b69b
Edward <eddiecorrigall@gmail.com>
2014-12-30 13:07:07 -0500

    Initial commit

---
+++ jquery.scroll.js
@@ -0,0 +1,37 @@
+(function ($) {
+
+	$.fn.scrollTo = function(target, settings, callback) {
+
+		// Based on: http://lions-mark.com/jquery/scrollTo/
+
+		settings = $.extend({
+			offsetTop:	64,
+			offsetLeft:	64,
+			// jQuery.animate parameters...
+			speed:		"slow",
+			easing:		"swing"
+		}, settings);
+
+		return this.each(function() {
+			
+			var scrollPane = $(this);
+
+			var scrollX = target.offset().left + scrollPane.scrollLeft() + parseInt(offsetLeft);
+			var scrollY = target.offset().top + scrollPane.scrollTop() + parseInt(offsetTop);
+
+			scrollPane.animate({
+					scrollLeft:	scrollX,
+					scrollTop:	scrollY
+				},
+				settings.speed,
+				settings.easing,
+				function() {
+					if (typeof callback == 'function') {
+						callback.call(this);
+					}
+				}
+			);
+		});
+	};
+
+}(jQuery));
